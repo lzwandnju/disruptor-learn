a598e1c8fd8b573b9b1cdcc69870e3e2f00bccd5
lzwandnju <872627178@qq.com>
2020-06-22 17:33:14 +0800

    #FSP-1# CONFIG

---
+++ src/test/java/com/dis/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dis.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
